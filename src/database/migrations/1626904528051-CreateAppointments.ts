a5501b58db3690de08e16d57591900265608059a
Gabriel Anadinho <gabrielanadinho@gmail.com>
2021-07-26 19:06:57 -0300

    Criacao de registros

--- src/database/migrations/1626904528051-CreateAppointments.ts
+++ src/database/migrations/1626904528051-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1626904528051 implements MigrationInterfa
           columns:[
             {
               name:'id',
-              type:'varchar',
+              type:'uuid',
               isPrimary:true,
               generationStrategy:'uuid',
               default:'uuid_generate_v4()',
