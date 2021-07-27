1c48e754d16e66e08a0c987dc88abe9423ff3e48
Gabriel Anadinho <gabrielanadinho@gmail.com>
2021-07-27 15:25:06 -0300

    Validando Credenciais

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,43 @@
+import { getRepository } from 'typeorm';
+import User from '../models/User';
+import {compare} from 'bcryptjs'
+
+interface Request {
+  email:string;
+  password:string;
+}
+
+interface Response{
+  user: User;
+}
+
+
+  class AuthenticateUserService{
+  public async execute ({ email, password}:Request): Promise<Response>{
+    const usersRepository = getRepository(User);
+
+    const user =  await usersRepository.findOne({ where: { email } });
+
+    if(!user){
+      throw new Error('Incorrect email/password combination');
+    }
+
+    // user.password - senha criptografada no banco
+    // password- senha recebida no front, não-criptografada
+    const passwordMatched = await compare(password, user.password);
+
+    if(!passwordMatched){
+      throw new Error('Incorrect email/password combination');
+    }
+
+    // chegou ate aqui, informações corretas e usuario autenticado!
+
+    return{
+      user,
+    };
+
+  }
+
+}
+
+export default AuthenticateUserService;
